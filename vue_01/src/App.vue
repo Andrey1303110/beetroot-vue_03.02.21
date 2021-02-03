3f546c9024c868a2132ac1366b4caf7a30e385d3
Andrey1303110 <andrey1303110@gmail.com>
2021-02-03 19:54:53 +0200

    slots examples

---
+++ vue_01/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+
+    <card v-for="card in cards" :key="card.title">
+      <template #header>
+        <h1>{{ card.title }}</h1>
+      </template>
+      <template #default>
+        <p :style="`color: ${card.color}`">{{ card.text }}</p>
+      </template>
+    </card>
+    
+  </div>
+</template>
+
+<script>
+import Card from "./components/Card";
+export default {
+  components: {
+    Card
+  },
+  data() {
+    return {
+      cards: [
+        {
+          title: "Card 1",
+          text: "Text for the Card 1",
+          color: "red",
+        },
+        {
+          title: "Card 2",
+          text: "Text for the Card 2",
+          color: "green",
+        },
+        {
+          title: "Card 3",
+          text: "Text for the Card 3",
+          color: "blue",
+        }
+      ]
+    }
+  },
+}
+</script>
---
+++ vue_01/src/components/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    text: String,
+  }
+}
+</script>
+
+<style scoped>
+div {
+  display: block;
+  width: 300px;
+  border: 1px solid black;
+  margin-bottom: 15px;
+  padding: 10px;
+}
+</style>
